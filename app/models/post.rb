50897dde79e29420d0d7a3332bceebec38dd3a5f
nabilhayet <nabil4457@yahoo.com>
2020-08-03 22:30:17 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,13 @@ class Post < ActiveRecord::Base
     validates :summary, length: { maximum: 250}
     validates :category, inclusion: { in: %w(Fiction Non-Fiction),
     message: "%{value} is not a valid category" }
+    validate :clickbait
+
+    def clickbait
+        if self.title.present?
+            unless self.title.match?(/Won't Believe | Secret | Top\s(0...9) | Guess/)
+                self.errors[:title] << "Title doesn't match"
+            end
+        end 
+    end
 end
